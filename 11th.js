2c6bce9f3bde3a464cd2bcdfaff356ec1c7b7d99
Mohit Negi <mnegi1433@gmail.com>
2024-09-16 10:03:48 +0530

    requestAnimationFrame

--- 11th.js
+++ 11th.js
@@ -1,4 +1,10 @@
-// requestAnimationFrame
+// requestAnimationFrame - 
+
+// it is used to create animations with good frame rate
+// it is battery friendly
+// the animations stop when the page is not active
+
+// refer to index.html to see how it works
 
 function step(timestamp){
     console.log(timestamp);
